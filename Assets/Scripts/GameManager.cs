64b8c2f55cc003db9bc0fd05a4c4d8cd84839f24
moirafish <greenu@foxmail.com>
2023-03-25 21:08:58 +0800

    add a game project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float resatertDelay = 2f;
+    public void EndGame()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("game over"); 
+            //instead of using
+            //Restart();
+            Invoke("Restart",resatertDelay );
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
